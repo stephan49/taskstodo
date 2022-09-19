89342812093d3b9bff95b7105bad47fe22844e89
stephan-t <40370954+stephan-t@users.noreply.github.com>
2022-09-19 19:28:11 -0400

    Add module docs

--- tasklists.py
+++ tasklists.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 
+"""
+Create, read, update or delete task lists.
+"""
+
 import json
 
 from googleapiclient.discovery import build
--- tasks.py
+++ tasks.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 
+"""
+Create, read, update or delete tasks.
+"""
+
 import tasklists
 
 from googleapiclient.discovery import build
