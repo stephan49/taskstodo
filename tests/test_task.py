3ca4430c590c97086d05e68f20dfbee19c1583c4
stephan-t <40370954+stephan-t@users.noreply.github.com>
2022-10-25 19:27:50 -0400

    Add test for creating task

---
+++ tests/test_task.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+
+import unittest
+import os
+import sys
+import tasklists
+import tasks
+
+from io import StringIO
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+
+
+class TestTaskFunctions(unittest.TestCase):
+    """Test task functions."""
+
+    def authUser(self):
+        """Setup credentials to access API."""
+
+        SCOPES = ['https://www.googleapis.com/auth/tasks']
+
+        self.creds = None
+        if os.path.exists('token.json'):
+            self.creds = Credentials.from_authorized_user_file('token.json',
+                                                               SCOPES)
+        if not self.creds or not self.creds.valid:
+            if self.creds and self.creds.expired and self.creds.refresh_token:
+                self.creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                        'credentials.json', SCOPES)
+                self.creds = flow.run_local_server(port=0)
+            with open('token.json', 'w') as token:
+                token.write(self.creds.to_json())
+
+    def setUp(self):
+        """Setup test environment."""
+        self.authUser()
+        self.list_title = 'test list'
+        tasklists.create_tasklist(self.creds, self.list_title, False)
+
+        self.task_title = 'test task'
+
+        # Capture stdout to check correctness of output
+        self.output = StringIO()
+        sys.stdout = self.output
+
+    def test_create_task(self):
+        """Create task list with specified title."""
+        tasks.create_task(self.creds, self.list_title, self.task_title, None,
+                          -1, False)
+
+        tasklists.print_tasklist(self.creds, self.list_title, -1, False)
+
+        self.assertIn(f'0. {self.task_title}',
+                      self.output.getvalue().splitlines())
+
+    def tearDown(self):
+        """Cleanup test environment"""
+        self.output.truncate(0)
+
+        tasklists.print_all_tasklists(self.creds, 100, False)
+        num_lists = self.output.getvalue().splitlines().count(
+                f'- {self.list_title}')
+
+        self.output.close()
+
+        for _ in range(num_lists):
+            tasklists.delete_tasklist(self.creds, self.list_title, 0, False)
+
+
+if __name__ == '__main__':
+    unittest.main()
